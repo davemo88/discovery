240a303f11383bacd0e4b12690a56c6bde57ffd2
davemo88 <david@kasofsk.xyz>
2021-09-15 20:10:09 -0400

    09

--- src/09-clocks-and-timers/src/main.rs
+++ src/09-clocks-and-timers/src/main.rs
@@ -6,6 +6,10 @@ use aux9::{entry, switch_hal::OutputSwitch, tim6};
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
     // TODO implement this
+    tim6.arr.write(|w| w.arr().bits(ms));
+    tim6.cr1.modify(|_, w| w.cen().set_bit());
+    while !tim6.sr.read().uif().bit_is_set() {};
+    tim6.sr.modify(|_, w| w.uif().clear_bit());
 }
 
 #[entry]
@@ -14,6 +18,9 @@ fn main() -> ! {
     let mut leds = leds.into_array();
 
     // TODO initialize TIM6
+    rcc.apb1enr.write(|w| w.tim6en().set_bit());
+    tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
+    tim6.psc.write(|w| w.psc().bits(7_999));
 
     let ms = 50;
     loop {
